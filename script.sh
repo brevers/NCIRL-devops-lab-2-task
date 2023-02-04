41ddb569bf8c39de55077bddafe2f25bf248a5b4
Tamara Diaz <breversemail@gmail.com>
2023-02-04 10:16:10 +0000

    Point 8

---
+++ script.sh
@@ -0,0 +1,13 @@
+# Create folders fol_1 and fol_2
+mkdir fol_1
+mkdir fol_2
+
+# Create files in fol_1
+touch fol_1/1_1.txt
+touch fol_1/1_2.txt
+touch fol_1/1_3.txt
+
+# Create files in fol_2
+touch fol_2/2_1.txt
+touch fol_2/2_2.txt
+touch fol_2/2_3.txt
