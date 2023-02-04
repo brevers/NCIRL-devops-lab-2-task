1b6a3f29531c40e4663e18797c466011e645412c
Tamara Diaz <breversemail@gmail.com>
2023-02-04 10:46:26 +0000

    Update script to delete forlders before creating them

--- script.sh
+++ script.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+# Deleting folders
+rm -rf fol_1
+rm -rf fol_2
+
 # Create folders fol_1 and fol_2
 mkdir fol_1
 mkdir fol_2
@@ -11,3 +17,7 @@ touch fol_1/1_3.txt
 touch fol_2/2_1.txt
 touch fol_2/2_2.txt
 touch fol_2/2_3.txt
+
+# Creating .gitignore file
+echo "fol_1" >> .gitignore
+echo "fol_2" >> .gitignore
